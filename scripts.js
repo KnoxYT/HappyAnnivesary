b34c5180871cd3f1c46fa18011484702e1094534
KnoxYT <archibongraymond7@gmail.com>
2024-06-05 01:04:33 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("homepage").classList.remove("hidden");
+});
+
+function nextPage(pageId) {
+    const current = document.querySelector(".page:not(.hidden)");
+    current.classList.add("hidden");
+    document.getElementById(pageId).classList.remove("hidden");
+}
+
+function reject() {
+    const noButtons = document.querySelectorAll("button[id^='noButton']");
+    noButtons.forEach(button => {
+        const emojis = Array(5).fill('👉').join(' ');
+        button.innerHTML = emojis + ' Yes ' + emojis;
+    });
+}
